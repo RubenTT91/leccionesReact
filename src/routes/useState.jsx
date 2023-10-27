a1d3553686d80eb666c0b31d7c44a103c0b85358
Rubencho <Ruben.toro1@gmail.com>
2023-10-26 22:33:27 -0500

    Se agrega parrafo de useState

--- src/routes/useState.jsx
+++ src/routes/useState.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import '../routes/useState.css';
-import dinkelberg from '../img/mqdefault.jpg';
+import React from "react";
+import "../routes/useRef.css";
+import dinkelberg from "../img/mqdefault.jpg";
 
-
-function useState(){
+function useState() {
   return (
     <div className="contenedorPrincipal">
-        <p>Aqui vendría el useState si lo supiera manejar bien</p>
-        <img src={dinkelberg} alt='dinkelberg'/>
-        
+      <h1>useState</h1>
+      <p>
+        El useState, permite la renderización de manera automática de aquellas
+        variables que sufren un cambio. Por ejemplo Si tenemos una variable{" "}
+        <div className="cp">let variable=4;</div>. Cuando usamos una función,
+        que permita cambiar esa variable a través del método tradicional, a
+        pesar de que se haga el cambio, el nuevo valor no se va a renderizar.{" "}
+        <div className="cp">{`const function cambioarNumero =()=>{ variable = 10 }`}</div> <br/>
+        El nuevo valor de variable, será 10, pero no se va a ver reflejado. 
+      </p>
     </div>
-  )
+  );
 }
 
 export default useState;

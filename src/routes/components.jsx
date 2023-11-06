bbc57c496fa7ed874b1a0c35e5b02a8155f82051
Rubencho <Ruben.toro1@gmail.com>
2023-11-06 17:54:55 -0500

    Se finaliza página  por completo.

--- src/routes/components.jsx
+++ src/routes/components.jsx
@@ -13,31 +13,99 @@ function Components() {
         <strong>
           El componente más importante, es el componente{" "}
           <div className="cp"> app</div>, que es el que inicializa toda la
-          aplicación. </strong>{" "}
+          aplicación.{" "}
+        </strong>
         <br />
         Los componentes deben devolver código JSX o null. y puedo generar
-        cuantos componenes necesite mi proyecto, si los invoco dentro del proyecto,
-        no es necesario importarlos <br/>
-        <h3>Ejemplo</h3>
-        <div className="codigo">
-          {`const ComponenteUno =_ => {`}<br/>
-          {`return ( ` } <br/>
-          {`<div> Este es el componenteUno </div>`}<br/> 
-          {`)};`}<br/> 
-          <br/> 
-          <br/> 
-          {`cons App = _=> {`}<br/>
-          {`return (`}<br/>
-          {`<ComponenteUno />`}<br/>
-
-        </div>
-      </p>
-      <ol>
-        <p className="codigo">
-          {`import React from 'react';`}
-          {`import React from 'react'v;`}
+        cuantos componenes necesite mi proyecto, si los invoco dentro del
+        proyecto, no es necesario importarlos <br />
+        <br />
+        {/**1. Como usar el componente?
+         *
+         *
+        */}
+        <h2>¿Como usar un componente?</h2>
+        <p>
+          Como bien se sabe, un componente puede devolver o null o código JSX.
+          Por esto, es necesario importar react en el componente de la siguiente
+          manera <div className="cp">{`import React from 'react'; `} </div>
+          luego de construir el componente,{" "}
+          <div className="cp">{` const MyComponent() =>{<div>Code</div>>} `}</div>{" "}
+          finalmente, le daremos la instrucción de exportar
+          <div className="cp">{` export default MyComponente; `}</div>
         </p>
-      </ol>
+        <div className="codigo">
+          {`import React from 'react';`}<br /><br />
+          {`const Componente =_ => {`}<br />          
+          {`return ( `} <br />
+          {`<div>Este es el componenteUno </div>`}<br />
+          {`)};`}<br />
+          {`export default Componente;`}<br />
+
+          </div>
+        {/**2. Usar varios componentes
+         *
+         *
+         *
+        */}
+        <h2>Uso de varios componentes de diferentes ficheros</h2>
+        <p>
+          Para poder usar varios componentes, es necesario identificar si estan
+          en el mismo o en diferentes ficheros Si esta en el mismo archivo,
+          podremos usar el componente, sin necesidad de un{" "}
+          <div className="cp">export import</div><br />
+        </p>
+        <div className="codigo">
+          <br />
+          {`const MyComponente()=>{<div>Mi componente Uno</div>}`}< br/>< br/>
+          {`const MyComponenteDos()=>{<div>Mi componente Dos</div>}`}< br/>< br/>
+          {`const MyComponenteTres()=>{<div>Mi componente Tres</div>}`}< br/>< br/>
+          {`const MyComponentePrincipal()=>{ `}< br/>
+          {`return (`}< br/>
+          {`<div>Este es el componente Principal</div>`}< br/>
+          {`<MyComponente/>`}< br/>
+          {`<MyComponenteDos />`}< br/>
+          {`<MyComponenteTres />`}< br/>         
+          
+          {`)};`}< br/>< br/>
+        </div>
+          <p>
+            Como resultado tendríamos lo siguiente: <br/><br/>
+
+            Este es el componente Principal <br/>
+            Mi componente Uno <br/>
+            Mi componente Dos <br/>
+            Mi componente Tres<br/>
+            </p>          
+          <br />
+          <br />
+      
+        <div>
+          Ahora, en el mismo fichero, podemos usar otro componente, e incluso
+          invocar el componente anterior{" "}
+        </div>
+        <div className="codigo">
+          {`cons App = _=> {`}
+          <br />
+          {`return (`}
+          <br />
+          {`<div>`}
+          <br />
+          {`<ComponenteUno /> /** Estamos invocando el primer componente*/`}
+          <br />
+          {`<div>Y aca viene codigo del componente APP</div>`}
+          {`<div> `}
+          <br />
+        </div>
+        <h3>Resultado</h3>
+        <div>El resultado de este ejercicio, daría</div>
+      </p>
+
+      <p className="codigo">
+        {`Este es el componenteUno`}
+        {`Y acá viene el codigo del componente APP;`}
+      </p>
+      <h2>Como importarlo</h2>
     </div>
   );
 }
